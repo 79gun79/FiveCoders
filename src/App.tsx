f5718416b682d824f8acf1feeb2f5f972719250a
snowari <pes1149@naver.com>
2025-04-28 15:37:08 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Login from "./components/Login";
+
 export default function App() {
   return (
     <>
       <h1 className="text-3xl underline">FiveCoders</h1>
+      <Login />
     </>
   );
 }
