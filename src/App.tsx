d04509f3fbb027a96d35416497c2baf48934221d
SeolWolWha <leeyoon97@naver.com>
2025-05-03 00:51:31 +0900

    마이페이지 상단 컴포넌트화X

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,18 @@ import MyPage from "./pages/MyPage";
 export default function App() {
   return (
     <>
-      <MyPage />
+      <header className="border-1 min-w-screen h-[85px] bg-[var(--color-main)]">
+        상단 바
+      </header>
+      <main className="flex">
+        <aside className="border-1 w-[210px] min-h-screen bg-[var(--color-sub)]">
+          사이드 바
+        </aside>
+        <section className="w-[calc(100vw-210px)] mt-[42px]">
+          <MyPage />
+        </section>
+      </main>
+      <footer></footer>
     </>
   );
 }
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,58 +1,42 @@
 import profile from "../assets/imgs/리터.png";
+import setting from "../assets/icons/Setting.svg";
 
 export default function MyPage() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center relative p-[38px]">
-        <div className="w-[80%]">
-          <div>
-            <div className="flex">
-              <img
-                src={profile}
-                alt="profile"
-                className="rounded-full h-[100px] w-[100px] overflow-hidden object-fill mr-[18px]"
-              />
-              <div className="inline-block left-[100px] content-center">
-                <span className="block text-[12px]">
-                  게임에 쓴 돈이 전혀 궁금하지 않은
-                </span>
-                <span className="block text-[28px]">설월화</span>
-                <span className="block text-[16px]">@seolwolwha</span>
-              </div>
-              <div className="inline-grid ml-[20px]">
-                <div className="inline-flex border-2 rounded-[25px] p-[5px]">
-                  <div className="pr-[10px] pl-[10px] content-center">
-                    게시글
-                  </div>
-                  <div className="pr-[10px] border-r-[1px] content-center">
-                    40
-                  </div>
-                  <div className="pr-[10px] pl-[10px] content-center">
-                    팔로워
-                  </div>
-                  <div className="pr-[10px] border-r-[1px] content-center">
-                    10
-                  </div>
-                  <div className="pr-[10px] pl-[10px] content-center">
-                    팔로잉
-                  </div>
-                  <div className="pr-[10px] content-center">20</div>
+      <div className="flex flex-col items-center relative h-[100%] mt-[54px]">
+        <div>
+          <div className="flex">
+            <img
+              src={profile}
+              alt="profile"
+              className="rounded-full h-[90px] w-[90px] overflow-hidden object-fill mr-[18px]"
+            />
+            <div className="inline-block left-[100px] content-center">
+              <span className="block text-[28px]">설월화</span>
+              <span className="block text-[20px]">neung_3@naver.com</span>
+            </div>
+            <div className="inlin-grid ml-[46px] content-center">
+              <div className="flex border-[0.5px] rounded-[8px] h-[79px] w-[388.38px] text-center justify-center p-[7.11px] divide-x-[0.5px] divide-solid divide-[#9e9e9e]">
+                <div className=" w-[121.09px] h-[64.78px] content-center">
+                  <div className="text-[18px] m-[-3px]">게시글</div>
+                  <div className="text-[18px] font-bold">40</div>
                 </div>
-                <div className="inline-block border-2 rounded-[25px] pl-[10px] pt-[5px] pb-[5px] mt-[10px] content-center">
-                  히히 재밌어요
+                <div className="w-[121.09px] h-[64.78px] content-center">
+                  <div className=" text-[18px] m-[-3px]">팔로워</div>
+                  <div className="text-[18px] font-bold">10</div>
+                </div>
+                <div className="w-[121.09px] h-[64.78px] content-center">
+                  <div className="text-[18px] m-[-3px]">팔로잉</div>
+                  <div className="text-[18px] font-bold">20</div>
                 </div>
               </div>
             </div>
-            {/* 개인 프로필 정보 */}
+            <button className="ml-[81.62px] h-[32px]">
+              <img src={setting} alt="setting" />
+            </button>
           </div>
-          <div className="absolute top-[78px] right-[238px] text-[#51B8B2]">
-            설정
-          </div>
-          <div className="border-[0.5px] mt-[20px] mb-[20px]"></div>
-          <div className="mb-[20px]">User card area</div>
-          <div className="border-2 h-[90px] w-[220px] rounded-[20px] relative"></div>
-          <div className="h-[10px]"></div>
-          <div className="border-[0.5px] mt-[20px] mb-[20px]"></div>
+          {/* 개인 프로필 정보 */}
         </div>
       </div>
     </>
