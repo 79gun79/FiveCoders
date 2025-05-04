960ed0b5dc1766a75061d8dcbbe6b9c22fd28ec7
snowari <pes1149@naver.com>
2025-05-05 00:16:41 +0900

    💄style : 색상 변수 추가 정의 및 적용

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -93,14 +93,14 @@ export default function Sidebar() {
   }));
 
   return (
-    <aside className="w-[280px] h-screen border-r border-[#BDBDBD] bg-white flex flex-col">
+    <aside className="w-[280px] h-screen border-r border-[var(--color-gray4)] bg-white)] flex flex-col">
       <nav>
         <ul className="p-3">
-          <li className="flex items-center px-6 py-3 rounded-xl hover:bg-[#EEEEEE] cursor-pointer">
+          <li className="flex items-center px-6 py-3 rounded-xl hover:bg-[var(--color-gray2)] cursor-pointer">
             <VscHome className="w-5.5 h-5.5 mr-[13px] " />
             <span className="font-bold">홈</span>
           </li>
-          <li className="flex items-center px-6 py-3 rounded-xl hover:bg-[#EEEEEE] cursor-pointer">
+          <li className="flex items-center px-6 py-3 rounded-xl hover:bg-[var(--color-gray2)] cursor-pointer">
             <VscGlobe className="w-5.5 h-5.5 mr-[13px] " />
             <span className="font-bold">커뮤니티</span>
           </li>
@@ -108,16 +108,16 @@ export default function Sidebar() {
       </nav>
       <div className="flex-1 overflow-y-auto">
         <div className="flex items-center px-8 mt-[10px]">
-          <span className="flex-1 h-px bg-[#BDBDBD]" />
+          <span className="flex-1 h-px bg-[var(--color-gray4)]" />
         </div>
-        <h2 className="text-[16px] text-[#585858] px-8 py-2 mt-[25px]">
+        <h2 className="text-[16px] text-[var(--color-gray8)] px-8 py-2 mt-[25px]">
           즐겨찾는 커뮤니티
         </h2>
         <ul className="p-2.5">
           {channelItem.map((item) => (
             <li
               key={item.id}
-              className="flex items-center text-[16px] px-5.5 py-2.5 rounded-xl hover:bg-[#EEEEEE] cursor-pointer"
+              className="flex items-center text-[16px] px-5.5 py-2.5 rounded-xl hover:bg-[var(--color-gray2)] cursor-pointer"
             >
               <div className="w-8 h-8 mr-3 flex-shrink-0">
                 <img src={placeholderIcon} alt="icon" />
@@ -127,8 +127,8 @@ export default function Sidebar() {
                 <TiStarFullOutline
                   className={`text-[20px] transition-colors ${
                     item.isSubscribe
-                      ? "text-[#397C79] hover:text-[#51B8B2]"
-                      : "text-[#BDBDBD] hover:text-[#51B8B2]"
+                      ? "text-[var(--color-sub)] hover:text-[var(--color-main)]"
+                      : "text-[var(--color-gray4)] hover:text-[var(--color-main)]"
                   }`}
                 />
               </button>
