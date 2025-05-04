f2bf3ed87076613f01643df28e754b450f86bbdf
snowari <pes1149@naver.com>
2025-05-05 02:03:11 +0900

    ✨ feat : router 추가 및 로그인 비밀 번호 input 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import Sidebar from "./components/Sidebar";
-import Channel from "./pages/Channel";
+import { BrowserRouter } from "react-router";
+import Router from "./router";
 
 export default function App() {
   return (
-    <>
-      <header className="min-w-screen min-h-[80px] bg-[var(--color-main)]">
-        상단 바
-      </header>
-      <main className="flex">
-        <Sidebar />
-        <section className="w-auto mt-[50px] mx-[160px]">
-          <Channel />
-        </section>
-      </main>
-      <footer></footer>
-    </>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
