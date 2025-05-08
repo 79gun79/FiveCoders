617927c2ebad2664e8ffc12fc6ef7f95a85203ae
keemunji <kej20211496@gmail.com>
2025-05-08 12:48:27 +0900

    🎨 design: Layout에 Header 컴포넌트 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
 import Sidebar from './Sidebar';
 
 export default function Layout() {
     return (
         <div className="flex min-h-screen flex-col">
-            <header className="min-w-screen min-h-[80px] bg-[var(--color-main)]">
-                상단 바 {/* 나중에 Topbar 컴포넌트로 교체 */}
-            </header>
-
+            <Header />
             <div className="flex flex-1">
                 <Sidebar />
                 <main className="w-auto mt-[50px] mx-[160px]">
