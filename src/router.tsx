4a8710235d9c41d625a89c26770577b644660fa9
keemunji <kej20211496@gmail.com>
2025-05-08 11:15:03 +0900

    🔨 refactor: 공통 레이아웃 적용 및 중첩 라우터 구성

--- src/router.tsx
+++ src/router.tsx
@@ -1,21 +1,27 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
-import Channel from "./pages/Channel";
-import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
-import Post from "./pages/Post";
+import Channel from "./pages/Channel";
+import ChannelList from "./pages/ChannelList";
+import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
 
-//post는 퍼블리싱용 게시판 글 라우트 주소입니다.
-
-export default function router() {
+export default function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* 레이아웃 적용되지 않는 페이지 */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
-      <Route path="/channel" element={<Channel />} />
-      <Route path="/post" element={<Post />} />
+
+      {/* 레이아웃 적용되는 페이지 그룹 */}
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/channel" element={<Channel />} />
+        <Route path="/channellist" element={<ChannelList />} />
+      </Route>
+
+      {/* 404 */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
