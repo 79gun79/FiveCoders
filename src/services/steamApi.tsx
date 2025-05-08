345101250e9c5d9814c5b09b7e282b98f021a5d3
snowari <pes1149@naver.com>
2025-05-08 09:13:03 +0900

    ✨ feat : steam API 연결 및 카드 컴포넌트 생성

--- src/services/steamApi.tsx
+++ src/services/steamApi.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const STEAM_API_KEY = import.meta.env.VITE_STEAM_API_KEY;
-const BASE_URL = "http://api.steampowered.com";
+//const BASE_URL = "http://api.steampowered.com";
 
 export const getPlayerSummaries = async (steamIds: string[]) => {
   const ids = steamIds.join(",");
-  const url = `${BASE_URL}/ISteamUser/GetPlayerSummaries/v0002/`;
+  const url = `/steam-api/ISteamUser/GetPlayerSummaries/v0002/`;
   const params = {
     key: STEAM_API_KEY,
     steamids: ids,
@@ -13,5 +13,5 @@ export const getPlayerSummaries = async (steamIds: string[]) => {
   };
 
   const response = await axios.get(url, { params });
-  return response.data.respose.players;
+  return response.data.response.players;
 };
