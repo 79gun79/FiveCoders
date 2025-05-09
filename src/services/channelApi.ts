9f687a34566b8d2cfe843df40abc2dd6279c2dff
snowari <pes1149@naver.com>
2025-05-09 12:20:04 +0900

    ✨feat : 전체 채널 목록 불러오기 함수

---
+++ src/services/channelApi.ts
@@ -0,0 +1,7 @@
+import { client } from "./axios";
+import { Channel } from "../types/channel";
+
+export const fetchChannels = async (): Promise<Channel[]> => {
+  const { data } = await client.get("/channels");
+  return data;
+};
