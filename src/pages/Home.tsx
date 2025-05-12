2f70153227b1306e305bf23d8d6aa7b6c6cdb5af
snowari <pes1149@naver.com>
2025-05-12 22:23:46 +0900

    ✨ feat : discord api 함수 추가, discord card 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import DiscordCard from '../components/DiscordCard';
 import SteamCard from '../components/SteamCard';
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <div className="mx-[200px]">
         <h1>Home Component</h1>
         <SteamCard />
+        <DiscordCard />
       </div>
     </>
   );
