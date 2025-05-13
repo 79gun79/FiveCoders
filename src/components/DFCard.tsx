93171463f287171617d329abe7ebc4223830c0a4
SeolWolWha <leeyoon97@naver.com>
2025-05-13 21:53:01 +0900

    ✨ feat: 던파 유저카드 제작중

--- src/components/DFCard.tsx
+++ src/components/DFCard.tsx
@@ -1,44 +1,60 @@
 import { useEffect, useState } from 'react';
 import { getUser } from '../services/dfAPI';
+import logo_big from '../assets/던파로고.png';
+import logo_small from '../assets/던파로고미니.png';
 
-const myServer = 'hilder';
-const myNickname = '활쏘는설월화';
-const myCharacterId = 'd484c38866b960b1db8cc912a01fe594';
+const serverId = 'hilder';
+const charName = '변신캐설월화';
 
 export default function DFCard() {
-  // const [server, setServer] = useState<string>();
+  const [server, setServer] = useState<string>();
   // const [nickname, setNickname] = useState<string>();
   const [myFame, setMyFame] = useState<number>();
   const [myJob, setMyJob] = useState<string>();
   const [level, setLevel] = useState<number>();
-  // const [characterId, setCharacterId] = useState<string>();
+  const [characterId, setCharacterId] = useState<string>();
+
+  const serverName = () => {
+    if (serverId === 'hilder') {
+      setServer('힐더');
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const player = await getUser(myServer, myNickname);
+        const player = await getUser(serverId, charName);
         console.log(player);
-        setMyFame(player.fame);
-        setMyJob(player.jobGrowName);
-        setLevel(player.level);
+        setMyFame(player.rows[0].fame);
+        setMyJob(player.rows[0].jobGrowName);
+        setLevel(player.rows[0].level);
+        setCharacterId(player.rows[0].characterId);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
+    serverName();
   }, []);
 
   return (
     <>
       <h1>dfCard Component</h1>
-      <img
-        src={`https://img-api.neople.co.kr/df/servers/${myServer}/characters/${myCharacterId}?zoom=1`}
-        alt=""
-      />
-      <span>{myNickname}</span>
-      <span>{myFame}</span>
-      <span>{myJob}</span>
-      <span>{level}</span>
+      <div className="relative flex h-[160px] w-[260px] justify-start overflow-hidden rounded-xl border-1 bg-red-200 bg-gradient-to-r from-[#141E30] to-[#243B55] p-[10px]">
+        <img src={logo_small} className="absolute w-[35px]" alt="미니로고" />
+        <img
+          src={`https://img-api.neople.co.kr/df/servers/${serverId}/characters/${characterId}?zoom=1`}
+          alt=""
+          className="h-[130px]"
+        />
+        <div className="block text-white">
+          <span className="block">{charName}</span>
+          <span className="block">{server}</span>
+          <span className="block">명성치: {myFame}</span>
+          <span className="block">직업: {myJob}</span>
+          <span className="block">레벨: {level}</span>
+        </div>
+      </div>
     </>
   );
 }
--- src/services/dfAPI.ts
+++ src/services/dfAPI.ts
@@ -4,15 +4,14 @@ const DF_API_KEY = import.meta.env.VITE_DF_KEY;
 console.log(DF_API_KEY);
 
 export const getUser = async (serverId: string, charName: string) => {
-  const url = `/df-api/df/servers/:serverId/characters`;
+  const url = `/df-api/df/servers/${serverId}/characters/`;
   const params = {
-    server: serverId,
     characterName: charName,
-    key: DF_API_KEY,
+    apikey: DF_API_KEY,
     format: 'json',
   };
 
   const response = await axios.get(url, { params });
   console.log(response);
-  return response.data.rows;
+  return response.data;
 };
