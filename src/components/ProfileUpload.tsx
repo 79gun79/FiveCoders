84dd4437a230dc26bf7d99a9cf2251372965ec4d
snowari <pes1149@naver.com>
2025-05-14 23:51:49 +0900

    💄 style : profileSetting, input style 수정

--- src/components/ProfileUpload.tsx
+++ src/components/ProfileUpload.tsx
@@ -4,6 +4,7 @@ import prof from '../assets/imgs/기본 프로필.png';
 import axios from 'axios';
 import { client } from '../services/axios';
 import { useAuthStore } from '../stores/authStore';
+import { CiImageOn } from 'react-icons/ci';
 
 export default function ProfileUpload({
   userEmail,
@@ -72,7 +73,7 @@ export default function ProfileUpload({
     <>
       <img src={Image} alt="프로필" className="h-30 w-30 rounded-full" />
       <div className="ml-8.5">
-        <span className="textT1 block">이메일</span>
+        <span className="textT1 block text-[var(--color-gray7)]">이메일</span>
         <span className="textT1 mt-2.75 block">{userEmail}</span>
         <div className="h-3.75"></div>
         <input
@@ -85,8 +86,9 @@ export default function ProfileUpload({
         />
         <label
           htmlFor="profileImg"
-          className="textST1 cursor-pointer rounded-xl bg-[var(--color-gray1)] px-3 py-2.5"
+          className="textST1 cursor-pointer rounded-xl bg-[var(--color-gray1)] px-4 py-2.5 text-[var(--color-gray6)]"
         >
+          <CiImageOn className="mr-1 inline-block h-[16px] w-[16px] align-middle text-[var(--color-gray6)]" />
           프로필 사진 변경
         </label>
       </div>
