b2d32b1b501367813623ece7c24b324da93640d5
snowari <pes1149@naver.com>
2025-05-14 18:23:48 +0900

    ✨ feat : carousel login 추가 및 스타일링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ import SteamCard from '../components/SteamCard';
 export default function Home() {
   return (
     <>
-      <div className="mx-[200px]">
+      <div className="max-[1200px] mx-auto ml-[280px]">
         <h1>Home Component</h1>
         <div className="flex gap-1.5">
           <SteamCard />
@@ -14,7 +14,9 @@ export default function Home() {
           {/* <DFCard /> */}
         </div>
         <div className="flex items-center justify-center">
-          <GameCarousel />
+          <div className="m-[20px] w-[960px]">
+            <GameCarousel />
+          </div>
         </div>
       </div>
     </>
