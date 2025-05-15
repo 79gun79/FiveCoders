d04d1a5eaee4eb0ba5daf6b999f75d9c3d8e57be
79gun79 <79gun79@gmail.com>
2025-05-15 11:57:37 +0900

    🎨 design: 토스트 메시지 커스텀 추가!

--- src/components/PostEditor.tsx
+++ src/components/PostEditor.tsx
@@ -21,11 +21,9 @@ const PostEditor = forwardRef<ReactQuill, PostEditorProps>(
   ({ className, value, onChange, onImageChange }, ref) => {
     const [isFocused, setIsFocused] = useState(false);
 
-    // ✅ ReactQuill reference 생성
     const quillRef = useRef<ReactQuill>(null);
     useImperativeHandle(ref, () => quillRef.current!, []);
 
-    // ✅ 이미지 핸들러 정의
     const imageHandler = () => {
       const input = document.createElement('input');
       input.setAttribute('type', 'file');
@@ -41,7 +39,6 @@ const PostEditor = forwardRef<ReactQuill, PostEditorProps>(
       };
     };
 
-    // ✅ ReactQuill 모듈 설정
     const modules = useMemo(() => {
       return {
         toolbar: {
--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css'; // 토스트 메시지 적용을
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ToastContainer />
+    <ToastContainer limit={1} />
     <App />
   </StrictMode>,
 );
