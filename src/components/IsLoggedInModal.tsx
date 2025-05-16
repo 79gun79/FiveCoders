48cafe4ea92ddb40591db763d68759c972dc840b
79gun79 <79gun79@gmail.com>
2025-05-16 14:13:15 +0900

    💄 style: 코드 포맷팅

--- src/components/IsLoggedInModal.tsx
+++ src/components/IsLoggedInModal.tsx
@@ -17,14 +17,18 @@ export default function IsLoggedInModal({ onClose }: { onClose: () => void }) {
       />
       <div className="fixed inset-0 z-[1001] flex items-center justify-center">
         <div className="w-[400px] rounded-[8px] bg-white p-8 text-center shadow-lg">
-          <p className="text-[18px] font-medium">로그인이 필요한 서비스입니다</p>
-          <p className="mb-[32px] text-[18px] font-medium">로그인 하시겠습니까?</p>
+          <p className="text-[18px] font-medium">
+            로그인이 필요한 서비스입니다
+          </p>
+          <p className="mb-[32px] text-[18px] font-medium">
+            로그인 하시겠습니까?
+          </p>
           <div className="flex justify-center gap-4">
             <Button
               onClick={onClose}
               className={twMerge(
                 'btn-style-modal',
-                'border border-[var(--color-gray4)] bg-white text-[var(--color-text-black)] hover:bg-[var(--color-gray1)]'
+                'border border-[var(--color-gray4)] bg-white text-[var(--color-text-black)] hover:bg-[var(--color-gray1)]',
               )}
             >
               아니오
@@ -36,6 +40,6 @@ export default function IsLoggedInModal({ onClose }: { onClose: () => void }) {
         </div>
       </div>
     </>,
-    document.body
+    document.body,
   );
 }
