baca755ee7c9c29b9171bb652854068e02b2ed45
79gun79 <79gun79@gmail.com>
2025-05-19 14:52:51 +0900

    🐛 fix: 채널 목록 선택 버튼 정리

--- src/components/ChooseCommunity.tsx
+++ src/components/ChooseCommunity.tsx
@@ -1,4 +1,4 @@
-import { TiStarFullOutline } from 'react-icons/ti';
+// import { TiStarFullOutline } from 'react-icons/ti';
 import { twMerge } from 'tailwind-merge';
 import { useEffect, useState } from 'react';
 import { IoIosList } from 'react-icons/io';
@@ -38,12 +38,12 @@ export default function ChooseCommunity({
           'absolute top-13 right-0 z-20 flex flex-col text-[12px] font-medium text-[var(--color-text-black)]',
         )}
       >
-        <div className="flex items-center gap-[6px]">
+        {/* <div className="flex items-center gap-[6px]">
           <TiStarFullOutline className="text-[var(--color-sub)]" size={17} />
           <span className="leading-[30px]">즐겨찾는 커뮤니티</span>
         </div>
         <ul className="gap-[7px] px-1 pb-[10px] text-[13px] font-normal"></ul>
-        <hr className="mr-[20px] border border-[var(--color-gray3)]" />
+        <hr className="mr-[20px] border border-[var(--color-gray3)]" /> */}
         <div className="mt-1 flex items-center gap-[6px]">
           <IoIosList className="text-black" size={17} />
           <span className="leading-[30px]">전체 커뮤니티</span>
