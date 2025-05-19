1550501658753ecc08599a72be4cf1571ff39dd6
79gun79 <79gun79@gmail.com>
2025-05-19 22:53:47 +0900

    🐛 fix: 충돌 문제 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-=======
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
 import { useSubscriptionStore } from './stores/subscriptionStore';
 import { getSubscribedChannels } from './utils/localSubscribe';
 import { useEffect } from 'react';
 
->>>>>>> dev
 export default function App() {
   const setSubscribes = useSubscriptionStore((state) => state.setSubscribes);
   useEffect(() => {
@@ -14,7 +11,9 @@ export default function App() {
   }, [setSubscribes]);
   return (
     <>
-      <h1 className="text-3xl underline">FiveCoders</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </>
   );
 }
