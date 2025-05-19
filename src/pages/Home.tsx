4af2fa699e258a8540aea5846ce3c9a53f58fb62
SeolWolWha <leeyoon97@naver.com>
2025-05-19 09:30:00 +0900

    🐛 bug: 누락된 코드 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -50,7 +50,7 @@ export default function Home() {
     };
     document.addEventListener('mousedown', handler);
     return () => document.removeEventListener('mousedown', handler);
-  }, [showAddCard]);
+  }, [showAddCard, isOpen]);
 
   useEffect(() => {
     if (isLogin === true) {
